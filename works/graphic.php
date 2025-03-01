d32d8a57dd8b7e67c1c4315ebcfe0a1038dffcdc
Sota Tabu <mackerel711@gmail.com>
2025-03-01 15:35:55 +0900

    Delete -- 不要なページの削除

--- works/graphic.php
+++ works/graphic.php
@@ -5,8 +5,6 @@
 <div class="container-fluid">
     <div class="row">
         <a href="./graphic/opennakameguro/opennakameguro.php"><div class="col-md-12"><h1>Open Nakameguro</h1></div></a>
-        <a href="./graphic/tina2022happybag/tina2022happybag.php"><div class="col-md-12"><h1>TINA : JOJUN 2022 HAPPY BAG</h1></div></a>
-        <a href="./graphic/tina2023happybag/tina2023happybag.php"><div class="col-md-12"><h1>TINA : JOJUN 2023 HAPPY BAG</h1></div></a>
         <a href="./graphic/BBB/BBB.php"><div class="col-md-12"><h1>BLACK BLINK BLACK</h1></div></a>
         <a href="./graphic/be/be.php"><div class="col-md-12"><h1>be design studio</h1></div></a>
         <a href="./graphic/messagefromflowers/messagefromflowers.php"><div class="col-md-12"><h1>message from flowers</h1></div></a>
--- works/webdesign.php
+++ works/webdesign.php
@@ -4,9 +4,7 @@
 <body>
 <div class="container-fluid">
     <div class="row">
-        <a href="./webdesign/tinaApp/tinaApp.php"><div class="col-md-12"><h1>TINA : JOJUN App</h1></div></a>
         <a href="./webdesign/points/points.php"><div class="col-md-12 lastContent"><h1>Points</h1></div></a>
-
     </div>
 </div>
 </body>
