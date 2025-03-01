cc049572d9c726fb9a68274d47e138815dd06618
Sota Tabu <mackerel711@gmail.com>
2025-03-01 22:53:16 +0900

    Fix -- 各メニュー内のページの順番を変更

--- works/civi.php
+++ works/civi.php
@@ -4,8 +4,8 @@
 <body>
 <div class="container-fluid">
     <div class="row">
-        <a href="./civi/Fujisento/Fujisento.php"><div class="col-md-12"><h1>藤井銭湯</h1></div></a>
-        <a href="./civi/ONLYLOVEYOU/ONLYLOVEYOU.php"><div class="col-md-12 lastContent"><h1>ONLY LOVE YOU</h1></div></a>
+        <a href="./civi/ONLYLOVEYOU/ONLYLOVEYOU.php"><div class="col-md-12"><h1>ONLY LOVE YOU</h1></div></a>
+        <a href="./civi/Fujisento/Fujisento.php"><div class="col-md-12 lastContent"><h1>藤井銭湯</h1></div></a>
     </div>
 </div>
 </body>
--- works/graphic.php
+++ works/graphic.php
@@ -4,16 +4,16 @@
 <body>
 <div class="container-fluid">
     <div class="row">
-        <a href="./graphic/opennakameguro/opennakameguro.php"><div class="col-md-12"><h1>Open Nakameguro</h1></div></a>
-        <a href="./graphic/BBB/BBB.php"><div class="col-md-12"><h1>BLACK BLINK BLACK</h1></div></a>
-        <a href="./graphic/be/be.php"><div class="col-md-12"><h1>be design studio</h1></div></a>
-        <a href="./graphic/messagefromflowers/messagefromflowers.php"><div class="col-md-12"><h1>message from flowers</h1></div></a>
-        <a href="./graphic/nomessage/nomessage.php"><div class="col-md-12"><h1>no message</h1></div></a>
-        <a href="./graphic/campbells/campbells.php"><div class="col-md-12"><h1>Campbells season greeting</h1></div></a>
-        <a href="./graphic/elua/elua.php"><div class="col-md-12"><h1>elua</h1></div></a>
-        <a href="./graphic/assemm/assemm.php"><div class="col-md-12"><h1>assemm</h1></div></a>
+        <a href="./graphic/shiozakifoods/shiozakifoods.php"><div class="col-md-12"><h1>塩崎製餡所</h1></div></a>
         <a href="./graphic/businesscard/businesscard.php"><div class="col-md-12"><h1>Business card - Hair stylist</h1></div></a>
-        <a href="./graphic/shiozakifoods/shiozakifoods.php"><div class="col-md-12 lastContent"><h1>塩崎製餡所</h1></div></a>
+        <a href="./graphic/assemm/assemm.php"><div class="col-md-12"><h1>assemm</h1></div></a>
+        <a href="./graphic/elua/elua.php"><div class="col-md-12"><h1>elua</h1></div></a>
+        <a href="./graphic/campbells/campbells.php"><div class="col-md-12"><h1>Campbells season greeting</h1></div></a>
+        <a href="./graphic/nomessage/nomessage.php"><div class="col-md-12"><h1>no message</h1></div></a>
+        <a href="./graphic/messagefromflowers/messagefromflowers.php"><div class="col-md-12"><h1>message from flowers</h1></div></a>
+        <a href="./graphic/be/be.php"><div class="col-md-12"><h1>be design studio</h1></div></a>
+        <a href="./graphic/BBB/BBB.php"><div class="col-md-12"><h1>BLACK BLINK BLACK</h1></div></a>
+        <a href="./graphic/opennakameguro/opennakameguro.php"><div class="col-md-12 lastContent"><h1>Open Nakameguro</h1></div></a>
     </div>
 </div>
 </body>
