aecf84c2046becbe526182be94eae6915edd5901
Sota Tabu <mackerel711@gmail.com>
2025-03-01 13:59:02 +0900

    Add -- 塩崎製餡のページを追加

---
+++ works/tmp/shiozaki/shiozaki.php
@@ -0,0 +1,69 @@
+<?php require('../../common/php/works-header.php'); ?>
+
+<link rel="stylesheet" href="./shiozaki.css">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2 col-10 offset-1">
+            <div class="row">
+                <div class="col-md-6 col-12 description">
+                    <h1>Shiozaki foods</h1>
+                    <p>　</p>
+                    <p>Saiko Hayashi</p>
+                    <p>I designed a business card for a long-established bean paste manufacturing company, Shiozaki foods.
+                    We wanted to convey the goodness of Japanese sweets to young people, so I created a catchy design.
+                    </p>
+                    <p>
+                    The font used for the business card was designed to be simpler and less wasteful, with an image of cleanliness when handling food.
+                    </p>
+                    <p>
+                    老舗製餡所・有限会社塩﨑製餡所の名刺を作成。
+                    若年層にも和菓子の良さを伝えたいという思いから、キャッチーなデザインに。
+                    </p>
+                    <p>
+                    名刺に使用したフォントは、食品を取り扱う清潔感をイメージし、よりシンプルで無駄のない文字を作成。
+                    </p>
+                </div>
+                <div class="col-md-6 col-12">
+                    <img class="icon" src="./images/shioanco_icon.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-12 shioanco-card-container">
+                    <img class="shioanco-card" src="./images/shioanco_card1.jpg">
+                </div>
+                <div class="col-md-6 col-12">
+                    <img class="shioanco-card" src="./images/shioanco_card2.jpg">
+                </div>
+            </div>
+            <div class="row shioanco-container-row">
+                <div class="col-md-12 col-12">
+                    <img class="shioanco-long" src="./images/shioanco_date.png">
+                </div>
+            </div>
+            <div class="row shioanco-container-row">
+                <div class="col-md-12 col-12">
+                    <img class="shioanco-long" src="./images/shioanco_font.png">
+                </div>
+            </div>
+            <div class="row shioanco-container-row">
+                <div class="col-md-12 col-12">
+                    <img class="shioanco-long" src="./images/shioanco_obi.png">
+                </div>
+            </div>
+            <div class="row shioanco-container-row">
+                <div class="col-md-12 col-12">
+                    <img class="shioanco-smile" src="./images/shioanco_smile.png">
+                </div>
+            </div>
+            <div class="row shioanco-container-row">
+                <div class="col-md-12 col-12">
+                    <img class="shioanco-long" src="./images/shioanco_nobori.png">
+                </div>
+            </div>   
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
