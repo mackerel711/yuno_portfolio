cb1701b51685421c0148d71189926835868f81d1
Sota Tabu <mackerel711@gmail.com>
2025-03-01 15:34:21 +0900

    Improve -- 追加したページを所定のディレクトリに配置

--- works/logo.php
+++ works/logo.php
@@ -5,6 +5,7 @@
 <div class="container-fluid">
     <div class="row">
         <a href="./logo/brandingspace/brandingspace.php"><div class="col-md-12"><h1>Branding Space</h1></div></a>
+        <a href="./logo/handwork/handwork.php"><div class="col-md-12 lastContent"><h1>HANDWORK</h1></div></a>
     </div>
 </div>
 </body>
