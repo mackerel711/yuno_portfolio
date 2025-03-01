94d09c6d3e63b253c70c7d60385cf7c4463a940d
Sota Tabu <mackerel711@gmail.com>
2025-03-01 13:41:27 +0900

    Add -- saikohayashi名刺のページを追加

---
+++ works/tmp/businesscard/businesscard.php
@@ -0,0 +1,42 @@
+<?php require('../../common/php/works-header.php'); ?>
+
+<link rel="stylesheet" href="./businesscard.css">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2 col-10 offset-1">
+            <div class="row">
+                <div class="col-md-6 col-12 description">
+                    <h1>Business card - Hair stylist</h1>
+                    <p>　</p>
+                    <p>Saiko Hayashi</p>
+                    <p>Designed a business card for Saiko Hayashi, who works as a hair stylist. Since she works as a freelancer in a wide range of fields, from magazines to advertisements, she expresses her atmosphere with an axolotl illustration so that you can remember her in any situation.
+                    </p>
+                    <p>
+                        ヘアスタイリストとして活躍する林彩虹の名刺をデザイン。雑誌から広告まで幅広くフリーランスとして活躍するため、あらゆる現場で覚えていただけるよう、ウーパールーパーのイラストで林の雰囲気を表現。
+                        「彩虹（さいこ）」の名前からウーパールーパーの配置は3×5＝15で315（さいこ）を意識。
+                    </p>
+                </div>
+                <div class="col-md-6 col-12">
+                    <img class="icon" src="./images/saikohayashi_icon.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-12">
+                    <img class="saikohayashi" src="./images/saikohayashi_date.jpg">
+                </div>
+                <div class="col-md-6 col-12">
+                    <img class="saikohayashi" src="./images/saikohayashi_date2.jpg">
+                </div>
+            </div>
+            <div class="row saikohayashi-date-container-row">
+                <div class="col-md-12 col-12">
+                    <img class="saikohayashi-date" src="./images/saiko_date.png">
+                </div>
+            </div>           
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
