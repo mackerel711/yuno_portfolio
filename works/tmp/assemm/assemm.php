8ffa66ca1d23fdc4f8c0ecce69ec2885f179e445
Sota Tabu <mackerel711@gmail.com>
2025-03-01 13:22:28 +0900

    Add -- assemmのページを追加

---
+++ works/tmp/assemm/assemm.php
@@ -0,0 +1,43 @@
+<?php require('../../common/php/works-header.php'); ?>
+
+<link rel="stylesheet" href="./assemm.css">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2 col-10 offset-1">
+            <div class="row">
+                <div class="col-md-6 col-12 description">
+                    <h1>assemm</h1>
+                    <p>　</p>
+                    <p>I was in charge of creating the shop cards for the product brand assemm and the design cards that go with them.
+                    I envisioned a design that would blend in with products that are part of everyday life.
+                    </p>
+                    <p>
+                        プロダクトブランドassemmのショップカードと、同封するデザインカードの制作を担当。
+                        日常生活に溶け込むプロダクトに馴染むデザインをイメージ。
+                    </p>
+                </div>
+                <div class="col-md-6 col-12">
+                    <img class="icon" src="./images/assemm_icon.png">
+                </div>
+            </div>
+            <div class="row assemm-card-container-row">
+                <div class="col-md-6 col-12 assemm-card-container">
+                    <img class="assemm-card" src="./images/assemm_card2.png">
+                </div>
+                <div class="col-md-6 col-12 assemm-card-container">
+                    <img class="assemm-card" src="./images/assemm_card1.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-12 assemm-obi-container">
+                    <img class="assemm-obi" src="./images/assemm_obi.png">
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
