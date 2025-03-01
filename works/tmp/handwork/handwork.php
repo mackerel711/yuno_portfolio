c59781ee07c52e3c5f27f1ad11803c9e4c97e56a
Sota Tabu <mackerel711@gmail.com>
2025-03-01 13:33:38 +0900

    Add -- HANDWORKのページを追加

---
+++ works/tmp/handwork/handwork.php
@@ -0,0 +1,45 @@
+<?php require('../../common/php/works-header.php'); ?>
+
+<link rel="stylesheet" href="./handwork.css">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2 col-10 offset-1">
+            <div class="row">
+                <div class="col-md-6 col-12 description">
+                    <h1>HANDWORK</h1>
+                    <p>　</p>
+                    <p>I was in charge of the design of "HANDWORK," an event space for pop-ups and galleries that opened in Kabuto-cho Nisshokan in 2024.
+                    I created a logo that gives a gentle impression and can be used at the attached nail salon.
+                    </p>
+                    <p>
+                        2024年兜町 日証館にオープンした、ポップアップやギャラリーなどのイベントスペース「HANDWORK」のデザインを担当。
+                        併設のネイルサロンでも使用できるような柔らかい印象を与えるマークを制作。
+                    </p>
+                </div>
+                <div class="col-md-6 col-12">
+                    <img class="icon" src="./images/handwork_jote_icon.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-12">
+                    <img class="handwork" src="./images/handwork_jote1.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-12">
+                    <img class="handwork" src="./images/handwork_jote2.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-12">
+                    <img class="handwork" src="./images/handwork_jote_shop.JPG">
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
