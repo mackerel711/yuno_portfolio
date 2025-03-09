f765c476dadef91a107607636e4fe535aa37b33a
Sota Tabu <mackerel711@gmail.com>
2025-03-09 21:05:48 +0900

    Add -- ちよさんのおべんとうページを追加

--- works/logo.php
+++ works/logo.php
@@ -5,6 +5,7 @@
 <div class="container-fluid">
     <div class="row">
         <a href="./logo/handwork/handwork.php"><div class="col-md-12 newly-post"><img src="../images/new.png"><h1>HANDWORK</h1></div></a>
+        <a href="./logo/chiyobento/chiyobento.php"><div class="col-md-12 newly-post"><img src="../images/new.png"><h1>ちよさんのおべんとう</h1></div></a>
         <a href="./logo/brandingspace/brandingspace.php"><div class="col-md-12 lastContent"><h1>Branding Space</h1></div></a>
     </div>
 </div>
---
+++ works/logo/chiyobento/chiyobento.php
@@ -0,0 +1,40 @@
+<?php require('../../common/php/works-header.php'); ?>
+
+<link rel="stylesheet" href="./chiyobento.css">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2 col-10 offset-1">
+            <div class="row">
+                <div class="col-md-6 col-12 description">
+                    <h1>ちよさんのおべんとう</h1>
+                    <p>　</p>
+                    <p>I designed the logo for Chiyo-san's Bento, a bento shop that requires reservations.
+                        Packed into the limited size of a bento box, we used colorful polygons to depict the cooking process to express the nutritional balance and color of the ingredients.
+                    </p>
+                    <p>
+                        予約制お弁当店「ちよさんのお弁当」のロゴデザイン制作を担当。
+                        お弁当という限られたサイズの中に詰め込む、栄養バランスや食材の彩を表現するため、調理の様子をカラフルなポリゴンで表現。
+                    </p>
+                </div>
+                <div class="col-md-6 col-12 icon-container">
+                    <img class="icon" src="./images/chiyosan.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-12">
+                    <img class="chiyosan" src="./images/chiyosan.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-12">
+                    <img class="chiyosan" src="./images/chiyosan_photo.png">
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
